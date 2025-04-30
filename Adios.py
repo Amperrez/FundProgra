3fa4abc66c5c55eb2b54e7a4b76878437c3200bf
Amperrez <andresprz136@gmail.com>
2025-04-30 09:23:04 -0700

    Add files via upload

---
+++ Adios.py
@@ -0,0 +1,12 @@
+
+numero = int(input("Ingresa un número: "))
+
+if numero % 5 == 0:
+    print(f"{numero} es divisible entre 5")
+else:
+    print(f"{numero} no es divisible entre 5")
+    
+for i in range(1, numero + 1):
+        print(i)
+        
+        
