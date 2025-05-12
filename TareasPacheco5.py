d6b654ad7e18d71f9d89c7cdf965c4b3c4739ecb
Amperrez <andresprz136@gmail.com>
2025-05-11 21:05:04 -0700

    Add files via upload

---
+++ TareasPacheco5.py
@@ -0,0 +1,12 @@
+
+def PosONeg(Numero):
+
+    if (Numero > 0):
+        print("El numero", Numero, "es Positivo.")
+    else:
+         print("El numero", Numero, "es Negativo.")
+
+
+PosONeg(2.71)
+
+
