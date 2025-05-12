83ebb3635256a31ccdf18e5fee9ea47e5018f9d3
Amperrez <andresprz136@gmail.com>
2025-05-11 20:52:44 -0700

    Add files via upload

---
+++ TareasPacheco4.py
@@ -0,0 +1,15 @@
+
+
+def MismoValor(N1, N2):
+
+    if (N1 == N2):
+        print(N1, "y", N2, "tienen el mismo valor.")
+    if (N1 > N2):
+        print(N1, "Es mayor que", N2)
+    else:
+        print(N2, "Es mayor que", N1)
+
+MismoValor(4, 7)
+
+
+
