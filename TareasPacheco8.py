1012d6e2aa4c2962a71109bd235698fb9d5d9961
Amperrez <andresprz136@gmail.com>
2025-05-12 19:42:52 -0700

    Add files via upload

---
+++ TareasPacheco8.py
@@ -0,0 +1,13 @@
+
+Calf = float(input("Introduce el valor de Calf: "))
+
+if Calf < 7 and Calf >= 0:
+        print("CALIFICACION INSUFICIENTE")
+elif Calf == 7:
+        print("CALIFICACION SUFICIENTE")
+elif Calf > 7:
+        print("CALIFICACION SOBRESALIENTE")
+
+
+
+
